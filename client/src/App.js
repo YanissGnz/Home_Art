5ad56047e7ce8e8508e507f92ab6bd913c9d0d5b
Yaniss <m.guendouzi@esi-sba.dz>
2021-04-30 00:35:41 +0100

    added Client login

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import AdminLogin from "./screens/AdminLogin";
+import ClientLogin from "./screens/ClientLogin";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#F58634",
+		},
+	},
+});
 
 export default function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/admin">
-					<AdminLogin />
-				</Route>
-			</Switch>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Switch>
+					<Route exact path="/admin">
+						<AdminLogin />
+					</Route>
+					<Route exact path="/login">
+						<ClientLogin />
+					</Route>
+				</Switch>
+			</Router>
+		</ThemeProvider>
 	);
 }
